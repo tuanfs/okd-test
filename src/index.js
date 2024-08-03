9b95b18aee8d9e68ab4e88d3ce5f47fe72808f04
tuanlv51 <tuanlv51@fpt.com>
2024-08-03 20:55:41 +0700

    Update change port 80 to 3200

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import {startCronJobs} from './job.js';
 
 const app = express();
-const port = 80;
+const port = 3200;
 
 app.get('/', (req, res) => {
   console.log(`Call form job ${new Date()}`);
